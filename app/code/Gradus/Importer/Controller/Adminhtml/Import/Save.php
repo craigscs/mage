6d79bf97ee73eb250a9db78e9a648a894f5880c7
Craig Standish <craigscs@gradusgroup.com>
2017-02-16 16:27:14 -0500

    Updates to importer

---
+++ app/code/Gradus/Importer/Controller/Adminhtml/Import/Save.php
@@ -0,0 +1,58 @@
+<?php
+namespace Gradus\Importer\Controller\Adminhtml\Blog;
+
+use Magento\Backend\App\Action;
+
+class Save extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @param Action\Context $context
+     */
+    public function __construct(Action\Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     * Save action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($data) {
+            $model = $this->_objectManager->create('Gradus\Importer\Model\Import');
+
+            $id = $this->getRequest()->getParam('import_id');
+            if ($id) {
+                $model->load($id);
+            }
+
+            $model->setData($data);
+
+            try {
+                $model->save();
+                $this->messageManager->addSuccess(__('The import has been run.'));
+                $this->_objectManager->get('Magento\Backend\Model\Session')->setFormData(false);
+                if ($this->getRequest()->getParam('back')) {
+                    return $resultRedirect->setPath('*/*/edit', ['import_id' => $model->getId(), '_current' => true]);
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\RuntimeException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('Something went wrong while running this import.'));
+            }
+
+            $this->_getSession()->setFormData($data);
+            return $resultRedirect->setPath('*/*/edit', ['import_id' => $this->getRequest()->getParam('import_id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+}
